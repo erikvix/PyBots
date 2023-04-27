ecfd31eafc8197b0dff05886e50327c351208ee5
erikvix <erikvixx@gmail.com>
2023-04-27 06:48:15 -0300

    botv1

---
+++ test.py
@@ -0,0 +1,7 @@
+import pyautogui as bot
+from time import sleep
+
+# sleep(2)
+# iml = bot.screenshot(region=(100, 100, 1400, 800))
+# iml.save(r"C:\Users\Sejus\Desktop\pyautogui\saveimg.png")
+bot.displayMousePosition()
