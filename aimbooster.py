e5b4532d5b32b6abac27c7d99c506efb73d1a9a3
erikvix <erikvixx@gmail.com>
2023-04-30 20:38:09 -0300

    Cookie_Clicker_bot_V1

--- aimbooster.py
+++ aimbooster.py
@@ -3,27 +3,57 @@ import keyboard
 from time import sleep
 import win32api
 import win32con
-
-sleep(2)
+import schedule
 
 
 def click(x, y):
     win32api.SetCursorPos((x, y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
-    sleep(0.05)
+    sleep(0.02)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
 
 
-iml = bot.screenshot(region=(100, 100, 1400, 800))
-iml.save(r"C:\Users\Sejus\Desktop\pyautogui\saveimg.png")
+def clickCookie(c):
+    for x in range(int(c)):
+        bot.doubleClick(277, 419)
 
-# while keyboard.is_pressed('q') == False:
-#     pic = bot.screenshot(region=(100, 100, 1400, 800))
-#     width, height = pic.size
 
-#     for x in range(0, width, 5):
-#         for y in range(0, height, 5):
+def goldencookie():
+    position = bot.locateOnScreen(
+        'gc2.png', grayscale=True, confidence=0.8)
+    if position:
+        bot.moveTo(position[0] + 30, position[1] + 30)
+        bot.doubleClick()
 
-#             r, g, b = pic.getpixel((x, y))
-#             if r == 255:
-#                 click(x+100, y+100)
+
+def buyupgrade():
+    if bot.locateOnScreen('xd2.png', grayscale=False, confidence=0.8):
+        click(1251, 191)
+        for a in range(3):
+            for x in range(3):
+                click(1322, 300+x*65)
+
+
+bot.hotkey('alt', 'tab')
+while keyboard.is_pressed('q') == False:
+    clickCookie(20)
+
+    if bot.locateOnScreen('xd2.png', grayscale=False, confidence=0.7):
+        click(1251, 191)
+        for a in range(3):
+            for x in range(3):
+                click(1322, 300+x*65)
+        click(1322, 715)
+
+    position = bot.locateOnScreen(
+        'gc2.png', grayscale=True, confidence=0.8)
+    if position:
+        bot.moveTo(position[0] + 30, position[1] + 30)
+        bot.doubleClick()
+
+# *! GOLDEN COOKIE: var newShimmer=new Game.shimmer("golden");
+
+# *? TO-DO-LIST:
+# *? LOCALIZAR UPGRADE
+# *? DESENHAR UM RETÂNGULO VERDE EMCIMA DO UPGRADE
+# *? SE LOCALIZAR UPGRADE: CLIQUE
